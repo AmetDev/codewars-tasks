61f1e828a6caa2304cbd19f6108f70ef99b899b2
AmetJunior <ametsejdaliev@gmail.com>
2023-01-30 22:55:17 +0300

    30.01.2023-8-kyu-commit-task-й-no-last

---
+++ 8-kyu/7-Powers_of_2/index.js
@@ -0,0 +1,20 @@
+function powersOfTwo(n){
+let k = 0;
+ n.find((element) => {
+    k = element
+ })
+ console.log(k)
+ let q = [];
+ for(i = 0; i <= k; i++) {
+    q.push(k= k - 1)
+ }
+ const new1 =  q.reverse()
+ console.log(new1)
+
+  for(i = 0; i < k; i++) {
+
+  const new1 = Math.pow(n, i)
+  //console.log(new1)
+ }
+}
+powersOfTwo([5])
