a30f72486f093b23768d59c6cb6d6143d0b00894
AmetJunior <ametsejdaliev@gmail.com>
2023-02-20 23:00:28 +0300

    algorithms-and-data-structure_2.1-7-kyu

--- 7-kyu/1-Sort_by_binary_ones/index.js
+++ 7-kyu/1-Sort_by_binary_ones/index.js
@@ -1,23 +1,19 @@
 function sortByBinaryOnes(list){
     // your code here
+
     newarr = [];
     list.forEach(element => {
         newarr.push((element).toString(2));
       });
     console.log(newarr);
-    let start = 0;
-    let count = 0;
-    let end = newarr.length; 
-    let middle;
-    let found = false;
-    let position = -1;
-    while(found === false && start <= end) {
-        count += 1;
-        middle = Math.floor((start+end)/2);
-        if(newarr.length === middle) {
-            
-        }
+    let ch = '1';
+    let new1 = [];
+    for(i = 0; i < newarr.length; i++) {
+      var count = [...newarr[i]].filter(x => x === ch).length;
+      new1.push(newarr[i] = count)
     }
+    console.log(new1)
+   
   }
 
-sortByBinaryOnes([19,15,5,7,3])
+sortByBinaryOnes([1,15,5,7,3,7])
